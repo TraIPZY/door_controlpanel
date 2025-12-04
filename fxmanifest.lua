06f0cc96aba773dc211a93b47a9e17b34cd59024
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-12-04 12:17:20 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'TRAPZY'
+description 'Control panel system for door using ox_doorlock'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
