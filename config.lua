3eec816da178a4c782cc35448e8e8783d7933d8a
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-12-04 12:17:48 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+-- Liste des ID des portes gérées par ox_doorlock
+Config.CellDoors = {
+    3, 4, 5, 6, 7, 8, 9, 10 -- change selon tes portes
+}
+
+-- Coordonnées exactes du panneau/ordinateur
+Config.PanelCoords = vector3(1776.2200, 2490.3960, 50.3198)
